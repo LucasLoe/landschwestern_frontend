379b8469e32db4a759bf71f663e5302b01f1b707
Lucas L <93840559+LucasLoe@users.noreply.github.com>
2023-01-01 18:46:41 +0100

    ContactCard image src fixed

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -5,7 +5,7 @@ export default function ContactCard() {
     return (
         <div id="contact-card" className="w-full rounded-xl bg-[color:var(--ls-yellow)] max-w-[600px] px-4 lg:px-0 pt-4 lg:pt-0 lg:max-w-none my-4 flex flex-col lg:flex-row shadow-xl">
             <Image
-                src="/../public/assets/jobs-wallpaper.jpg"
+                src="/assets/jobs-wallpaper.jpg"
                 width={700}
                 height={700}
                 className="rounded-xl lg:mx-4 lg:my-8 w-full lg:w-1/3 object-cover"
