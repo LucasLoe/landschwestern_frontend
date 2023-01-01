0f357141f678d88e63b467836676b640db05d51c
Lucas L <93840559+LucasLoe@users.noreply.github.com>
2023-01-01 21:02:04 +0100

    maintenance mode: on

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/((?!maintenance).*)",
+        destination: "/maintenance.js",
+        permanent: false,
+      },
+      //{
+      // source: "/maintenance.js",
+      // destination: "/",
+      // permanent: false,
+      //},
+    ];
+  },
 }
 
 module.exports = nextConfig
