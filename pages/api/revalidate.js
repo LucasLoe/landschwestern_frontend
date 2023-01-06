09f9c32b97de422b6dbf303fb375d2838c58ff7a
Lucas L <93840559+LucasLoe@users.noreply.github.com>
2023-01-06 20:43:59 +0100

    revalidate first try

---
+++ pages/api/revalidate.js
@@ -0,0 +1,28 @@
+import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook';
+
+const handler = async (req, res) => {
+
+    //authenticating the webhook
+    try {
+        const signature = req.headers[SIGNATURE_HEADER_NAME].toString();
+        if (
+            !isValidSignature(
+                JSON.stringify(req.body),
+                signature,
+                process.env.SANITY_WEBHOOK_SECRET
+            )
+        )
+            return res.status(401).json({ msg: 'Invalid request!' });
+
+        //getting payload
+        console.log(req.body)
+        const { page } = req.body._type;
+        await res.revalidate(`/${page}`);
+
+        res.status(200).json({ msg: 'Pages revalidated.' });
+    } catch (error) {
+        res.status(500).json({ err: 'Something went Wrong!' });
+    }
+};
+
+export default handler;
