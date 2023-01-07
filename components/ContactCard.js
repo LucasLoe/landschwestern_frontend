d3085168edbbdc8df1de899e22951640b0029ce7
Lucas L <93840559+LucasLoe@users.noreply.github.com>
2023-01-07 22:34:04 +0100

    senioren image slider + portable text working

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -2,7 +2,7 @@ import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 import ContactForm from "./ContactForm";
 
-export default function ContactCard({title, text, imageSrc}) {
+export default function ContactCard({ title, text, imageSrc }) {
     return (
         <div id="contact-card" className="w-full rounded-xl bg-[color:var(--ls-yellow)] max-w-[600px] px-4 lg:px-0 pt-4 lg:pt-0 lg:max-w-none my-4 flex flex-col lg:flex-row shadow-xl">
             <Image
@@ -14,7 +14,9 @@ export default function ContactCard({title, text, imageSrc}) {
             />
             <div className='"h-full w-full my-auto mx-auto lg:mx-4 lg:my-8'>
                 <h3 className="text-center lg:text-left text-xl mb-4 mt-4 lg:mt-0 text-[color:var(--ls-blue)] font-bold box-border">{title}</h3>
-                <PortableText className="text-center lg:text-left" value={text} />
+                <div className="text-center lg:text-left">
+                    <PortableText value={text} />
+                </div>
             </div>
             <div className="h-full w-full my-auto mx-auto lg:mx-4 py-4">
                 <ContactForm />
--- pages/seniorenwohngemeinschaft.js
+++ pages/seniorenwohngemeinschaft.js
@@ -32,29 +32,29 @@ export default function SeniorCare(data) {
             <Navbar />
             <main>
                 <Wallpaper srcString={urlFor(data.data.image.asset._ref).url()} />
-                <div className='max-w-6xl mx-auto flex flex-col lg:flex-row flex-wrap justify-center px-4 lg:px-8 lg:justify-between'>
+                <div className='mt-4 max-w-6xl mx-auto flex flex-col lg:flex-row flex-wrap justify-center px-4 lg:px-8 lg:justify-between'>
+                    {
+                        data.data.galleryTextTop &&
+                        <div className="text-center my-8 mx-auto block w-full max-w-[896px]">
+                            <PortableText value={data.data.galleryTextTop} />
+                        </div>
+                    }
+                    {
+                        data.data.gallery &&
+                        <div className="mx-auto w-full md:max-w-[896px] flex justify-center my-8  mx-auto h-[450px] lg:h-[600px] rounded">
+                            <Carousel images={imageGalleryUrls()} />
+                        </div>
+                    }
+                    {
+                        data.data.galleryTextBottom &&
+                        <div className="text-center my-8 mx-auto block w-full max-w-[896px]">
+                            <PortableText value={data.data.galleryTextBottom} />
+                        </div>
+                    }
                     <SingleFacedCard title={data.data.boxHeading_1} text={data.data.boxSubText_1} imageSrc={urlFor(data.data.boxImage_1.asset._ref).url()} />
                     <SingleFacedCard title={data.data.boxHeading_2} text={data.data.boxSubText_2} imageSrc={urlFor(data.data.boxImage_2.asset._ref).url()} />
                     <SingleFacedCard title={data.data.boxHeading_3} text={data.data.boxSubText_3} imageSrc={urlFor(data.data.boxImage_3.asset._ref).url()} />
                     <SingleFacedCard title={data.data.boxHeading_4} text={data.data.boxSubText_4} imageSrc={urlFor(data.data.boxImage_4.asset._ref).url()} />
-                    {
-                        data.data.galleryTextTop && 
-                        <p className='text-center my-8'>
-                            {data.data.galleryTextTop}
-                        </p>
-                    }
-                    {
-                        data.data.gallery &&
-                        <div className="mx-auto w-full md:w-3/4 flex justify-center my-8 h-[350px] lg:h-[500px] rounded">
-                            <Carousel images={imageGalleryUrls()} />
-                        </div>
-                    }
-                                        {
-                        data.data.galleryTextBottom && 
-                        <p className='text-center my-8'>
-                            {data.data.galleryTextTop}
-                        </p>
-                    }
                     <div className='mx-auto'>
                         <ContactCard title={data.data.contactCardHeading} text={data.data.contactCardText} imageSrc={urlFor(data.data.contactCardImage.asset._ref).url()} />
                     </div>
