a715bb46246fbd30e16f37451260290c82ab891c
Lucas L <93840559+LucasLoe@users.noreply.github.com>
2023-01-14 10:17:04 +0100

    onclick event for DFC

--- components/DoubleFacedCard.js
+++ components/DoubleFacedCard.js
@@ -6,11 +6,33 @@ import { useState } from "react"
 export default function DoubleFacedCard({ title, text, icon, hoverText }) {
     const [isOpen, setIsOpen] = useState(true)
 
+    function useWindowSize() {
+        const [windowSize, setWindowSize] = useState(undefined);
+
+        useEffect(() => {
+            function handleResize() {
+                setWindowSize(window.innerWidth);
+            }
+
+            window.addEventListener("resize", handleResize);
+            handleResize();
+
+            return () => window.removeEventListener("resize", handleResize);
+        }, []);
+        return windowSize;
+    }
+
+    function handleOpenState() {
+        if (useWindowSize >= 1024) {
+            setIsOpen(!isOpen)
+        }
+    }
+
     return (
         <div className="relative rounded-2xl bg-[color:var(--ls-blue)] min-w-[300px] w-1/3 h-[580px] m-4 shadow-2xl text-center ">
             {
                 isOpen ?
-                    <div id="is-not-clicked" className="h-full cursor-pointer px-4" onMouseEnter={() => { setIsOpen(!isOpen) }} onClick={() => { setIsOpen(!isOpen) }} >
+                    <div id="is-not-clicked" className="h-full cursor-pointer px-4" onMouseEnter={() => { setIsOpen(!isOpen) }} onClick={() => { handleOpenState }} >
                         <FontAwesomeIcon icon={icon} className="fa fa-5x w-[100px] h-[100px] text-white mx-auto my-10" />
                         <h1 className="text-2xl text-[color:var(--ls-white)] font-extrabold mx-auto" >
                             {title}
@@ -18,12 +40,12 @@ export default function DoubleFacedCard({ title, text, icon, hoverText }) {
                         <p className="text-white text-md my-4 ">
                             {text}
                         </p>
-                        <a id="mobile-open-link" className="absolute text-lg font-bold text-white opacity-50 bottom-0 w-content m-3 right-0 lg:hidden cursor-pointer">
+                        <a id="mobile-open-link" className="absolute text-lg font-bold text-white opacity-50 bottom-0 w-content m-3 right-0 cursor-pointer">
                             {"[Mehr erfahren]"}
                         </a>
                     </div>
                     :
-                    <div id="is-clicked" className="h-full px-4 cursor-pointer border-8 border-[color:var(--ls-yellow)] bg-white rounded-2xl" onMouseLeave={() => { setIsOpen(!isOpen) }} onClick={() => { setIsOpen(!isOpen) }}>
+                    <div id="is-clicked" className="h-full px-4 cursor-pointer border-8 border-[color:var(--ls-yellow)] bg-white rounded-2xl" onMouseLeave={() => { setIsOpen(!isOpen) }} onClick={() => { handleOpenState }}>
                         <FontAwesomeIcon icon={icon} className="absolute left-0 right-0 top-[25%] fa fa-10x text-[color:var(--ls-blue)] opacity-10 mx-auto my-10" />
                         <h1 className="text-2xl text-[color:var(--ls-blue)] my-8 font-extrabold mx-auto">
                             {title}
